65e0dc1285702b4665d868c79b672d9bc83cc5f2
Matheus <matheus.barboza.henrique@gmail.com>
2020-09-02 17:36:41 -0300

    finalizado

---
+++ Interfaces/IAluno.cs
@@ -0,0 +1,18 @@
+﻿using API_Boletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastar(Aluno a);
+
+        List<Aluno> lerTodos();
+        Aluno BuscarPorId(int id);
+        Aluno Alterar(Aluno a);
+        Aluno Excluir(Aluno a);
+    }
+}
